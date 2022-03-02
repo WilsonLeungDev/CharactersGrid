b7a40fcb18a53aee0117df15c92ace5c147a75e4
Wilson Leung <wilsonleung0914@gmail.com>
2022-03-02 13:48:53 +0800

    Initial Commit

---
+++ CharactersGrid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CharactersGrid
+//
+//  Created by Wilson Leung on 2/3/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
